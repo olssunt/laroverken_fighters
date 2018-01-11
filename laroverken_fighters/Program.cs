f754764d1f79a86d6ef1fa8de7d0f0ce2da03eb2
olssunt <olssunt@users.noreply.github.com>
2018-01-11 15:23:06 +0100

    added enemy class

--- laroverken_fighters/Program.cs
+++ laroverken_fighters/Program.cs
@@ -8,24 +8,27 @@ namespace laroverken_fighters
 {
     class Program
     {
+        static Enemy enemy = new Enemy ();
+
+        
+
         static Random randomness = new Random();
         static int playerHP = randomness.Next(10, 20);
-        static int enemyHP = randomness.Next(7, 17);
 
         static int playerDmg = randomness.Next(2, 6);
-        static int enemyDmg = randomness.Next(1, 5);
-
         static string userInput;
 
 
         static void Main(string[] args)
         {
+            enemy.Setup();       
+
             Console.Title = "Läroverket Fighters";
             Console.SetWindowSize(32, 12);
 
 
             //Game loop. As long as both lives, do this
-            while (playerHP > 0 && enemyHP > 0)
+            while (playerHP > 0 && enemy.hp > 0)
             {
                 Console.Clear();
 
@@ -41,7 +44,7 @@ namespace laroverken_fighters
                 if (userInput == "1") //attack
                 {
                     playerDmg = randomness.Next(3, 8);
-                    enemyHP -= playerDmg;
+                    enemy.hp -= playerDmg;
 
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("Player attacked for " + playerDmg);
@@ -70,11 +73,11 @@ namespace laroverken_fighters
             } //end of while loop
 
             //if we are here someone died
-            if (enemyHP < 1)
+            if (enemy.hp < 1)
             {
                 //Enemy died died
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write("\nEnemy died!");
+                Console.Write("\n" + enemy.name + " died! ");
 
             }
             else
@@ -94,18 +97,18 @@ namespace laroverken_fighters
 
             {
                 int healAmount = randomness.Next(1, 3);
-                enemyHP += healAmount;
+                enemy.hp += healAmount;
                 Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine("Enemy healed for " + enemyDmg);
+                Console.WriteLine(enemy.name + " healed for " + enemy.dmg);
             }
             else
             {
                 //Enemy attacks
-                enemyDmg = randomness.Next(2, 7);
-                playerHP -= enemyDmg;
+                enemy.dmg = randomness.Next(2, 7);
+                playerHP -= enemy.dmg;
 
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Enemy attacked for " + enemyDmg);
+                Console.WriteLine(enemy.name + " attacked for " + enemy.dmg);
             }
         }
 
@@ -117,7 +120,7 @@ namespace laroverken_fighters
 
 
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Enemy HP:" + enemyHP);
+            Console.WriteLine(enemy.name + "'s HP: " + enemy.hp);
         }
     }
 }
---
+++ laroverken_fighters/enemy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laroverken_fighters
+{    //En class funkar som en beskrivning av någonting
+    //Här beskriver vi hur en fiende fungerar och vilka
+    //egenskaper den har
+    class Enemy
+    {
+        public int hp;
+        public int dmg;
+        public string name;
+        //Initiaize, sätt värden på variablerna
+        public void Setup()
+        {
+            Random randomness = new Random();
+            hp = randomness.Next(6, 15);
+            dmg = randomness.Next(2, 6);
+
+            string[] namesToPick =
+            {
+                "Christina", //0
+                "Dan Nordell", //1
+                "Skolverket", //2
+                "CSN" //3
+
+            };
+
+            name = namesToPick[randomness.Next(0, namesToPick.Length)];
+
+        }        
+    }
+}
