b63417c36cb05215b0350fea3bfa496679067a9e
olssunt <olssunt@users.noreply.github.com>
2018-01-18 15:12:40 +0100

    Added forloop

---
+++ For loops Csharpskolan/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_loops_Csharpskolan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Övning 1
+            //for (int i = 0; i < 101; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Övning 2
+
+            //for (int i = 20; i > 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.Read();
+
+            for (int i = 0; i < 52; i = i + 2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
